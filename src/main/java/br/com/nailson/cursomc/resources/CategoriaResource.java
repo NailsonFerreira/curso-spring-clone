d515c769ca0847774cdc413b41a981ac82f80160
Nailson Ferreira <nailson_ferreira@hotmail.com>
2021-02-19 14:30:25 -0300

    Aula 20 - Add Exception Handler

--- src/main/java/br/com/nailson/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/nailson/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package br.com.nailson.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,14 +21,7 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
 		Categoria obj = service.buscar(id);
-		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Cama");
-		Categoria cat3 = new Categoria(3, "Mesa");
-		List<Categoria> list = new ArrayList<>();
-		list.add(cat1);
-		list.add(cat2);
-		list.add(cat3);
+	
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/br/com/nailson/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/nailson/cursomc/services/CategoriaService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import br.com.nailson.cursomc.domain.Categoria;
 import br.com.nailson.cursomc.repositories.CategoriaRepository;
+import br.com.nailson.cursomc.services.exception.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -17,6 +19,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
