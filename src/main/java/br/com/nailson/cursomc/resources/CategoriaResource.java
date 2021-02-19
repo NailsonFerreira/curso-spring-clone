27121fc3902819047c3091bc560fee6faf77df50
Nailson Ferreira <nailson_ferreira@hotmail.com>
2021-02-19 00:37:33 -0300

    Aula 15 - First domain class

--- src/main/java/br/com/nailson/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/nailson/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package br.com.nailson.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.nailson.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Hello World";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Cama");
+		Categoria cat3 = new Categoria(3, "Mesa");
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		list.add(cat3);
+		return list;
 	}
 }
