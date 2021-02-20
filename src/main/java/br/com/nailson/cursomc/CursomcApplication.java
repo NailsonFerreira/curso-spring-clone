3f94491d3b0c4d69c8e399b5c04303b68605dacd
Nailson Ferreira <nailson_ferreira@hotmail.com>
2021-02-19 21:33:31 -0300

    Aula 21 - Estado e Cidade

--- src/main/java/br/com/nailson/cursomc/CursomcApplication.java
+++ src/main/java/br/com/nailson/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.nailson.cursomc.domain.Categoria;
+import br.com.nailson.cursomc.domain.Cidade;
+import br.com.nailson.cursomc.domain.Estado;
 import br.com.nailson.cursomc.domain.Produto;
 import br.com.nailson.cursomc.repositories.CategoriaRepository;
+import br.com.nailson.cursomc.repositories.CidadeRepository;
+import br.com.nailson.cursomc.repositories.EstadoRepository;
 import br.com.nailson.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -18,6 +22,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository catRepo;
 	@Autowired
 	private ProdutoRepository prodRepo;
+	@Autowired
+	private CidadeRepository cidRepo;
+	@Autowired
+	private EstadoRepository estRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -40,9 +48,22 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		
 		catRepo.saveAll(Arrays.asList(cat1, cat2));
 		prodRepo.saveAll(Arrays.asList(p1, p2,p3));
+
+		Estado e1 = new Estado(null, "Minas Gerais"); 
+		Estado e2 = new Estado(null, "São Paulo"); 
+		
+		Cidade ci1 = new Cidade(null, "Uberlandia", e1);
+		Cidade ci2 = new Cidade(null, "São Paulo", e2);
+		Cidade ci3 = new Cidade(null, "Campinas", e2);
+		
+		
+		e1.getCidades().addAll(Arrays.asList(ci1));
+		e2.getCidades().addAll(Arrays.asList(ci2, ci3));
+		
+		estRepo.saveAll(Arrays.asList(e1, e2));
+		cidRepo.saveAll(Arrays.asList(ci1, ci2, ci3));
 	}
 
 }
