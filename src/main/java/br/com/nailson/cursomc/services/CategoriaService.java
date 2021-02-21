b3452bb72f45690771c90222e14c29d52d4931d4
Nailson Ferreira <nailson_ferreira@hotmail.com>
2021-02-21 11:39:05 -0300

    Aula 34 - Post Categoria

--- src/main/java/br/com/nailson/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/nailson/cursomc/services/CategoriaService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import br.com.nailson.cursomc.domain.Categoria;
 import br.com.nailson.cursomc.repositories.CategoriaRepository;
 import br.com.nailson.cursomc.services.exception.ObjectNotFoundException;
@@ -21,5 +20,11 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		return obj;
 	}
 }
