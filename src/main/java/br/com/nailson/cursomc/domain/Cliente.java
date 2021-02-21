64699f8e7ae9cd188a68297744a477b74434c83f
Nailson Ferreira <nailson_ferreira@hotmail.com>
2021-02-21 20:38:33 -0300

    Aula 44- Deletando cliente

--- src/main/java/br/com/nailson/cursomc/domain/Cliente.java
+++ src/main/java/br/com/nailson/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -29,11 +30,11 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
-	@CollectionTable(name = "TETEFONE")
+	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
 	@JsonIgnore
