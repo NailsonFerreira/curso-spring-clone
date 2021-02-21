dd6bf09d8fd418a03d6226baf92f996ddb1ba509
Nailson Ferreira <nailson_ferreira@hotmail.com>
2021-02-21 17:57:31 -0300

    Aula 41 - PUT, DELETE, UPDATE para cliente

---
+++ src/main/java/br/com/nailson/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,59 @@
+package br.com.nailson.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.nailson.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	@NotEmpty(message = "Preenchimento obrigatoório")
+	@Length(min = 5, max = 120, message = "O nome deve conter entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatoório")
+	@Email
+	private String email;
+	
+	public ClienteDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClienteDTO(Cliente cli) {
+		id = cli.getId();
+		nome = cli.getNome();
+		email = cli.getEmail();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
